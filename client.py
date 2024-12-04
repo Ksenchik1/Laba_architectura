3ee94ded2d23a0a8a59ae4b9e413d7579fdeabed
Ksenchik1 <110306486+Ksenchik1@users.noreply.github.com>
2024-12-04 12:39:26 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,82 +1,43 @@
 import socket
-import threading
 
-server_running = True
-lock = threading.Lock()
-clients = []
-
-
-def close_server():
-    with lock:
-        global server_running
-        server_running = False
-        print("Сервер закрывается...")
-
-        for client in clients:
-            client.close()
-
-        server.close()
-        print("Сервер закрыт")
-
-
-def h_client(con, c_id):
-    global server_running
-
-    try:
-        while server_running:
-            data = con.recv(1024)
-            if not data:
-                print(f"Клиент {c_id} отключился.")
-                break
-
-            message = data.decode()
-            print(f"Клиент {c_id} ввел: {message}")
-
-            if message.lower() == 'ping':
-                response = "Pong"
-                con.send(response.encode())
-                print(f"Клиент {c_id} получил: {response}")
-            elif message.lower() == 'close':
-                print(f"Клиент {c_id} запросил закрытие сервера.")
-                con.send("Сервер закрывается...".encode())
-                close_server()                
-                break
-            else:
-                response = "Мне такое не нравится("
-                con.send(response.encode())
-                print(f"Клиент {c_id} получил: {response}")
-    except ConnectionResetError:
-        print(f"Соединение с клиентом {c_id} разорвано.")
-    except Exception as e:
-        print(f"Произошла ошибка от клиента {c_id}: {e}")
-    finally:
-        con.close() 
-
+client = socket.socket()
+hostname = socket.gethostname()
+port = 17085
 
 try:
-    server = socket.socket()
-    hostname = socket.gethostname()
-    port = 17085
-    ip = socket.gethostbyname(hostname)
+    client.connect((hostname, port))
+except ConnectionRefusedError:
+    print("Не удалось подключиться к серверу. Сервер может быть закрыт.")
+    client.close()
+    exit()
 
-    server.bind((hostname, port))
-    server.listen(2)
-    print("Сервер запущен")
-    print(f"Порт: {port}")
-    print(f"Хост: {hostname}")
-    print(f"Ip: {ip} \n")
+while True:
+    try:
+        message = input("Введите текст (введите 'close' для выхода): ")
+        if message.strip() == '':
+            print("Вы не можете отправить пустую строку. Пожалуйста, введите данные.")
+            continue
+        client.send(message.encode())
 
-    while server_running:
+        if message.lower() == 'close':
+            print("Закрытие соединения с сервером.")
+            data = client.recv(1024)
+            print("Сервер отправил:", data.decode())
+            break
+
         try:
-            client, _ = server.accept()
-            clients.append(client)
-            c_id = len(clients)
-            threading.Thread(target=h_client, args=(client, c_id)).start()
-            print(f"Клиент {c_id} подключился.")
+            data = client.recv(1024)
+        except OSError as e:
+            print("Соединение с сервером было прервано: ", e)
+            break
 
-        except Exception as e:
-            print(f"Ошибка при подключении клиента: {e}")
+        if not data:
+            print("Сервер разорвал соединение.")
+            break
 
-except OSError as e:
-    print(f"Произошла ошибка при создании сервера: {e}")
-    exit(1)
+        print("Сервер отправил:", data.decode())
+    except OSError as e:
+        print("Ошибка при отправке сообщения:", e)
+        break
+
+client.close()
